2620c502680a1a7e89b39cae69f32bb9fa6b0164
maiqamaj <71187949+maiqamaj@users.noreply.github.com>
2021-08-25 00:21:00 +0300

    Update GradesReportTest.java
    Task13

--- src/test/java/jo/secondstep/app/GradesReportTest.java
+++ src/test/java/jo/secondstep/app/GradesReportTest.java
@@ -1,20 +1,24 @@
 package jo.secondstep.app;
 
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
 
 public class GradesReportTest {
 	@Test
-	@DisplayName("Calcuate Stduent Average ")
-	public void calcuateStduentAverageTest() throws Exception {
+	@DisplayName("Calculate Stduent Average ")
+	public void calculateStduentAverageTest() throws Exception {
 		HashMap<String, Integer> grades = new HashMap<String, Integer>();
 		grades.put("Java", 60);
 		grades.put("C++", 90);
 		grades.put("C#", 75);
-		assertEquals(75.0, GradesReport.calcuateStduentAverage(grades));
+		assertEquals(75.0, GradesReport.calculateStduentAverage(grades));
 	}
 
 	@Test
@@ -26,7 +30,7 @@ public class GradesReportTest {
 		grades.put("C#", 200);
 
 		assertThrows(Exception.class, () -> {
-			GradesReport.calcuateStduentAverage(grades);
+			GradesReport.calculateStduentAverage(grades);
 		});
 
 	}
@@ -41,7 +45,7 @@ public class GradesReportTest {
 		grades.clear();
 		// assertEquals(Float.NaN, GradesReport.calcuateStduentAverage(grades));
 		assertThrows(Exception.class, () -> {
-			GradesReport.calcuateStduentAverage(grades);
+			GradesReport.calculateStduentAverage(grades);
 		});
 	}
 
@@ -54,7 +58,7 @@ public class GradesReportTest {
 		grades.put("C#", null);
 
 		assertThrows(NullPointerException.class, () -> {
-			GradesReport.calcuateStduentAverage(grades);
+			GradesReport.calculateStduentAverage(grades);
 		});
 
 	}
@@ -68,8 +72,23 @@ public class GradesReportTest {
 		grades.put("C++", 50);
 
 		assertThrows(Exception.class, () -> {
-			GradesReport.calcuateStduentAverage(grades);
+			GradesReport.calculateStduentAverage(grades);
 		});
+
+	}
+	@Test
+	@Timeout(value = 1, unit = TimeUnit.SECONDS)
+	public void PerformaneTest() throws Exception {
+		HashMap<String, Integer> grades = new HashMap<String, Integer>();
+		grades.put("Java", 90);		
+		grades.put("C++", 50);
+		grades.put("C#", 90);		
+		grades.put("Python", 50);
+		grades.put("Android", 90);		
+		grades.put("C", 50);
+     //for (long j=0;j<10000000;j++)
+     //{     GradesReport.calculateStduentAverage(grades); }
+     GradesReport.calculateStduentAverage(grades);
 
 	}
 }
